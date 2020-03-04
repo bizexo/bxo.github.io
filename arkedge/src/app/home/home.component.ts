cd30373a224d69d9bb90adaad387e22305953361
bizexo <41581712+bizexo@users.noreply.github.com>
2020-03-04 10:59:16 +0530

    arkedge-29-2-2020

---
+++ arkedge/src/app/home/home.component.ts
@@ -0,0 +1,121 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgbCarousel, NgbSlideEvent, NgbSlideEventSource } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent {
+
+  sevices;
+  solution;
+
+  constructor() {
+    this.sevices = [
+      {
+        title: 'IT, telecom and software',
+        color: 'skyblue',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet.',
+        icon: '#icon_ITBpo'
+      },
+      {
+        title: 'ITES - BPO & KPO',
+        color: 'yellow ',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet.',
+        icon: '#icon_customerService'
+      },
+      {
+        title: 'Engineering & Manufacturing',
+        color: 'orange',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet.',
+        icon: '#icon_engineeringMgf'
+      },
+      {
+        title: 'Consumer goods and retail',
+        color: 'limegreen',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet.',
+        icon: '#icon_consumerGood'
+      },
+      {
+        title: 'Financial services and insurance',
+        color: 'purple',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet.',
+        icon: '#icon_finervice'
+      },
+      {
+        title: 'Health care & Pharmaceuticals',
+        color: 'blue',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet.',
+        icon: '#icon_customerService'
+      },
+    ];
+    this.solution = [
+      {
+        title: 'Data Management',
+        color: 'red',
+        list : [
+          {name: 'Cleanse Dataset'},
+          {name: 'Data Mapping & Conversion'},
+          {name: 'Data Migration & Consolidation'},
+          {name: 'Spend Analysis'}
+        ],
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales pulvinar tempor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam fermentum, nulla luctus pharetra vulputate, felis tellus mollis orci, sed rhoncus sapien nunc eget odio',
+        icon: '#icon_mgmt'
+      },
+      {
+        title: 'Software Engineering',
+        color: 'blue',
+        list : [
+          {name: 'Product Engineering'},
+          {name: 'Application Development'},
+          {name: 'UX/UI Development'},
+          {name: 'Software Testing & QA'}
+        ],
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales pulvinar tempor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam fermentum, nulla luctus pharetra vulputate, felis tellus mollis orci, sed rhoncus sapien nunc eget odio',
+        icon: '#icon_softEngg'
+      },
+      {
+        title: 'IT Staffing & Training',
+        color: 'green',
+        list : [
+          {name: 'Staffing Services'},
+          {name: 'Industries'},
+          {name: 'Courses'},
+          {name: 'Program Calendar'}
+        ],
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales pulvinar tempor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam fermentum, nulla luctus pharetra vulputate, felis tellus mollis orci, sed rhoncus sapien nunc eget odio',
+        icon: '#icon_training'
+      },
+    ];
+  }
+
+  images = [62, 83, 466, 965, 982, 1043, 738].map((n) => `https://picsum.photos/id/${n}/900/500`);
+
+  paused = false;
+  unpauseOnArrow = false;
+  pauseOnIndicator = false;
+  pauseOnHover = true;
+
+  @ViewChild('carousel', { static: true }) carousel: NgbCarousel;
+
+  togglePaused() {
+    if (this.paused) {
+      this.carousel.cycle();
+    } else {
+      this.carousel.pause();
+    }
+    this.paused = !this.paused;
+  }
+
+  onSlide(slideEvent: NgbSlideEvent) {
+    if (this.unpauseOnArrow && slideEvent.paused &&
+      (slideEvent.source === NgbSlideEventSource.ARROW_LEFT || slideEvent.source === NgbSlideEventSource.ARROW_RIGHT)) {
+      this.togglePaused();
+    }
+    if (this.pauseOnIndicator && !slideEvent.paused && slideEvent.source === NgbSlideEventSource.INDICATOR) {
+      this.togglePaused();
+    }
+  }
+}
+
